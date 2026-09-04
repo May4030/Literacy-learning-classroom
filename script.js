c30bee0e0b93bec11e3e89544b243b169e7c2f16
May4030 <maygarcia05@gmail.com>
2026-09-04 10:08:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+const links = document.querySelectorAll("nav a");
 
+enlaces.forEach(función (enlace) {
+enlace.addEventListener("click", function (evento) {
+const targetId = this.getAttribute("href");
+
+if (targetId.startsWith("#")) {
+evento.preventDefault();
+
+const targetSection = document.querySelector(targetId);
+
+si (secciónobjetivo) {
+targetSection.scrollIntoView({
+comportamiento: "suave"
+});
+}
+}
+});
+});
+});
